8ce67375fe3e97c8ab5cef6f01b0a5ce9a8ae0fa
ali-bouali <bouali.pro@gmail.com>
2023-06-02 11:03:33 +0200

    shipping service

---
+++ services/shipping/src/test/java/com/bouali/shipping/ShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bouali.shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
