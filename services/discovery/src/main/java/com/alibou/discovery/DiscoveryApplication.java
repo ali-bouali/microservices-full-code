2871cf4c32abdf352e7973878c6faf99e78c491f
ali-bouali <bouali.pro@gmail.com>
2023-06-02 11:02:55 +0200

    discovery server

---
+++ services/discovery/src/main/java/com/alibou/discovery/DiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.alibou.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+}
---
+++ services/discovery/src/test/java/com/alibou/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
