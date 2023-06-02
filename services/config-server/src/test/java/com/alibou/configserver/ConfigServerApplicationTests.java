0c60b9700beb90353e3687cee3e9c0aed3947122
ali-bouali <bouali.pro@gmail.com>
2023-06-02 11:02:27 +0200

    config server

---
+++ services/config-server/src/test/java/com/alibou/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
