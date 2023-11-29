3c72bf55fd21351f3f87c308fce2089e354cce44
ali-bouali <bouali.pro@gmail.com>
2023-11-29 15:50:19 +0100

    init notification project

---
+++ services/notification/src/main/java/com/alibou/ecommerce/NotificationApplication.java
@@ -0,0 +1,13 @@
+package com.alibou.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApplication.class, args);
+	}
+
+}
